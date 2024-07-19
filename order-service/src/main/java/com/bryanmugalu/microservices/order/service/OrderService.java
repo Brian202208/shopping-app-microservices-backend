fa48c848a0e6c6f97e4a9ca9102639bd4ff9b09d
Your Name <you@example.com>
2024-07-18 20:57:03 -0500

    Order-Service completed

---
+++ order-service/src/main/java/com/bryanmugalu/microservices/order/service/OrderService.java
@@ -0,0 +1,24 @@
+package com.bryanmugalu.microservices.order.service;
+
+import com.bryanmugalu.microservices.order.dto.OrderRequest;
+import com.bryanmugalu.microservices.order.model.Order;
+import com.bryanmugalu.microservices.order.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setSkuCode(orderRequest.skuCode());
+        order.setPrice(orderRequest.price());
+        order.setQuantity(orderRequest.quantity());
+
+        orderRepository.save(order);
+    }
+}
